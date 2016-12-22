d0120e90593bdab76b8e94f449c8eb992b19d67b
blackratio <blackratio@gmail.com>
2016-12-22 22:42:25 +0100

    ios mirror copy from android

--- index.ios.js
+++ index.ios.js
@@ -6,48 +6,87 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+   AppRegistry,
+   StyleSheet,
+   TextInput,
+   Text,
+   ScrollView,
+   View,
+   Button,
+   Alert
 } from 'react-native';
 
+const onButtonPress = () => {
+   Alert.alert('HellYeah button has been pressed!');
+};
+
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+   render() {
+      return (
+         <View style={styles.wrapper}>
+            <View style={styles.header}>
+               <Text style={styles.welcome}>
+               Header - Welcome to React Native David!
+               </Text>
+            </View>
+            <View style={styles.content}>
+               <ScrollView>
+                  <Button onPress={onButtonPress} title="Press Me" accessibilityLabel="See an informative alert"/>
+                  <Text style={{fontSize:60}}>Scroll me plz</Text>
+                  <Text style={{fontSize:60}}>Scroll me plz</Text>
+                  <Text style={styles.awesome}>
+                  Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.
+                  </Text>
+                  <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here to translate!"
+                  onChangeText={(text) => this.setState({text})}
+                  />
+               </ScrollView>
+            </View>
+            <View style={styles.footer}>
+               <Text style={styles.instructions}>
+               Footer Baby
+               </Text>
+            </View>
+         </View>
+      );
+   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   wrapper: {
+      flex: 1
+   },
+   header: {
+      height: 50,
+      backgroundColor: '#444'
+   },
+   content: {
+      flex: 7,
+      padding: 20,
+      backgroundColor: '#fff'
+   },
+   footer: {
+      flex: 1,
+      backgroundColor: 'steelblue'
+   },
+   welcome: {
+      fontSize: 19,
+      textAlign: 'center',
+      padding: 10,
+      color: '#000'
+   },
+   awesome: {
+      fontSize: 12,
+      textAlign: 'center',
+      color: '#444',
+      margin: 10,
+   },
+   instructions: {
+      textAlign: 'center',
+      color: '#333333'
+   },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
